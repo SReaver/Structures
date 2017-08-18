5b6706de63c5b351292a725311162fa383968e21
ГерценЕ <@IT-227-TEACHER>
2017-08-18 22:00:58 +0600

    Add project files.

---
+++ Structures/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Сколько будет сотрудников: ");
+            int ans = Int32.Parse(Console.ReadLine());
+            List<Sotrudnik> LS = new List<Sotrudnik>();
+            for (int i = 1; i <= ans; i++)
+              {
+                Sotrudnik s = new Sotrudnik();
+                Console.Write("Введите имя сотрудника: ");
+                s.name = Console.ReadLine();
+                Console.Write("Введите должность сотрудника: ");
+                s.role = Console.ReadLine();
+                Console.Write("Введите зарплату сотрудника: ");
+
+                double tDouble=0;
+                double.TryParse(Console.ReadLine(), out tDouble);
+                s.salary = tDouble;
+                Console.Write("Введите дату устройства сотрудника: ");
+                DateTime dt = DateTime.Now;
+                DateTime.TryParse(Console.ReadLine(), out dt);
+                s.dateCome = dt;
+                LS.Add(s);
+                Console.WriteLine();
+
+            }
+
+            foreach (var item in LS)
+            {
+                item.sotrudnikInfo();
+            }
+
+            foreach (var item in LS)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ Structures/Sotrudnik.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures
+{
+    public class Sotrudnik
+    {
+        public string name { get; set; }
+        public string role { get; set; }
+        public double salary { get; set; }
+        public DateTime dateCome { get; set; }
+
+        public void sotrudnikInfo()
+        {
+            Console.WriteLine($"Имя: {name}\nФамилия: {role}\nЗарплата: {salary}\nДата устройства: {dateCome}");
+        }
+
+    }
+}
